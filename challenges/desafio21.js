4607884eba771f8a0b74d92a0b37183d7f40a984
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-15 21:54:50 -0300

    desafio21

---
+++ challenges/desafio21.js
@@ -0,0 +1,14 @@
+db.produtos.updateOne({
+  nome: "Cheddar McMelt",
+}, 
+{
+  $pop: {
+      ingredientes: 1,
+  },
+});
+
+db.produtos.find({}, {
+  _id: false,
+  nome: 1,
+  ingredientes: 1,
+});
